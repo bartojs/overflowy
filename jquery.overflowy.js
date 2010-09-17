852f470dd7f27cd25f8aae651aab8f3cafc49634
justin barton <justin@.(none)>
2010-09-17 15:00:13 +1200

    add resetHeight setting so that height is auto resized if not overflowing vertically

--- jquery.overflowy.js
+++ jquery.overflowy.js
@@ -1,11 +1,15 @@
 /* overflowy: a really simple jquery plugin for handling vertical overflow. http://github.com/bartonj/overflowy */
 (function($) {
-    $.fn.overflowy = function() {
+    $.fn.overflowy = function(settings) {
         return this.each(function() {
             $(this).css('overflow-y', 'scroll');
             if (this.clientHeight < this.scrollHeight)
             {
                 $(this).after('<div class="ellipsis" title="' + $.trim($(this).text()) + '">...</div>');
+            }
+            else if (settings && settings.resetHeight)
+            {
+                $(this).css('height', 'auto');
             }
             $(this).css('overflow-y', 'hidden');
         });
